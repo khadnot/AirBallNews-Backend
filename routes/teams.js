d21ab302bce96ce21a6a7e2bfb1877cec6433e8c
Kenn Hadnot <k_hadnot@yahoo.com>
2021-07-16 23:35:38 -0700

    added home page to teams route

--- routes/teams.js
+++ routes/teams.js
@@ -7,6 +7,10 @@ import express from 'express';
 
 const router = new express.Router();
 
+router.get('/', async function (req, res, next) {
+    return res.json('Air Ball News Home');
+})
+
 router.get('/:team', async function (req, res, next) {
     try {
         let team = await Team.getTeam(req.params.team);
